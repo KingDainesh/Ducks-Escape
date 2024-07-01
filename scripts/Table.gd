31899730104ff3c94cdb9c3cd02332db2c4ae774
SaxySam <21140@burnside.school.nz>
2024-07-01 16:05:56 +1200

    Put things in right place puzzle, untested

---
+++ scripts/Table.gd
@@ -0,0 +1,35 @@
+extends MeshInstance3D
+
+var Capsule = false
+var Prism = false
+var Box = false
+
+func body_processing(entered, area, body_rid, body, body_shape_index, local_shape_index):
+	print(entered, area, body_rid, body, body_shape_index, local_shape_index)
+	print(body.Class)
+	if body.Class:
+		if str(body.Class) == "Capsule" and entered == true: Capsule = true
+		elif str(body.Class) == "Capsule": Capsule = false
+		elif str(body.Class) == "Prism" and entered == true: Prism = true
+		elif str(body.Class) == "Prism": Prism = false
+		elif str(body.Class) == "Box" and entered == true: Box = true
+		elif str(body.Class) == "Box": Box = false
+		if Capsule == true and Prism == true and Box == true: self.queue_free()
+
+func _on_prism_body_shape_entered(body_rid, body, body_shape_index, local_shape_index):
+	body_processing(true, "Prism", body_rid, body, body_shape_index, local_shape_index)
+
+func _on_capsule_body_shape_entered(body_rid, body, body_shape_index, local_shape_index):
+	body_processing(true, "Capsule", body_rid, body, body_shape_index, local_shape_index)
+
+func _on_cube_body_shape_entered(body_rid, body, body_shape_index, local_shape_index):
+	body_processing(true, "Cube", body_rid, body, body_shape_index, local_shape_index)
+
+func _on_prism_body_shape_exited(body_rid, body, body_shape_index, local_shape_index):
+	body_processing(false, "Prism", body_rid, body, body_shape_index, local_shape_index)
+
+func _on_capsule_body_shape_exited(body_rid, body, body_shape_index, local_shape_index):
+	body_processing(false, "Capsule", body_rid, body, body_shape_index, local_shape_index)
+
+func _on_cube_body_shape_exited(body_rid, body, body_shape_index, local_shape_index):
+	body_processing(false, "Cube", body_rid, body, body_shape_index, local_shape_index)
