46968a6c546d0ad0d251cad17d594841da18e352
BURNSIDE HIGH SCHOOL <code@burnside.school.nz>
2024-07-22 14:40:47 +1200

    ah

--- scripts/Table.gd
+++ scripts/Table.gd
@@ -5,17 +5,17 @@ var Prism = false
 var Box = false
 
 func body_processing(entered, area, body_rid, body, body_shape_index, local_shape_index):
-	print(body_rid)
-	print(body.name)
+	print(body)
 	print(body.has_meta("Shape"))
 	print(body.get_meta("Shape"))
-	if body.has_meta():
-		if str(body.get_meta()) == "capsule" and entered == true: Capsule = true
-		elif str(body.Metadata.Class) == "capsule": Capsule = false
-		elif str(body.Metadata.Class) == "prism" and entered == true: Prism = true
-		elif str(body.Metadata.Class) == "prism": Prism = false
-		elif str(body.Metadata.Class) == "box" and entered == true: Box = true
-		elif str(body.Metadata.Class) == "box": Box = false
+	if body.has_meta("Shape"):
+		var shape = body.get_meta("Shape")
+		if shape == "capsule" and entered == true: Capsule = true
+		elif shape == "capsule": Capsule = false
+		elif shape == "prism" and entered == true: Prism = true
+		elif shape == "prism": Prism = false
+		elif shape == "box" and entered == true: Box = true
+		elif shape == "box": Box = false
 		if Capsule == true and Prism == true and Box == true: self.queue_free()
 
 func _on_prism_body_shape_entered(body_rid, body, body_shape_index, local_shape_index):
