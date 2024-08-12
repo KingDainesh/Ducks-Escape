dbbc188a74bbe4556b6639ec32073cd2c6359361
SaxySam <21140@burnside.school.nz>
2024-08-13 11:25:26 +1200

    All I know is pain!

--- scripts/Poster.gd
+++ scripts/Poster.gd
@@ -2,5 +2,6 @@ extends RigidBody3D
 
 
 func _on_area_3d_body_entered(body):
+	print(body)
 	if "LeftHand" in str(body.find_parent("LeftHand")) or "LeftHand" in str(body.find_parent("LeftHand")):
 		self.freeze = false
