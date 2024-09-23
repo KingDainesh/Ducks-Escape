088f2b8b3ce3a77b33b80d4fed563321db61b6b6
BURNSIDE HIGH SCHOOL <code@burnside.school.nz>
2024-09-24 08:51:49 +1200

    religiouszone

--- scences/religious-offering.gd
+++ scences/religious-offering.gd
@@ -3,7 +3,7 @@ extends Area3D
 
 
 func _on_body_entered(body: Node3D) -> void:
-	print("OH HAIL" + str(body))
+	print("OH HAIL" + str(body.get_parent().name))
 
 
 func _on_body_exited(body: Node3D) -> void:
