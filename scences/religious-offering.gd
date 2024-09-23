f1e97ef7d1fe643749e98087f3b693eb0f519bf0
BURNSIDE HIGH SCHOOL <code@burnside.school.nz>
2024-09-24 09:25:18 +1200

    BreadContributionWorks

--- scences/religious-offering.gd
+++ scences/religious-offering.gd
@@ -1,10 +1,18 @@
 extends Area3D
 
 
-
-func _on_body_entered(body: Node3D) -> void:
-	print("OH HAIL" + str(body.get_parent().name))
+var breadcount = 0
 
 
-func _on_body_exited(body: Node3D) -> void:
-	pass # Replace with function body.
+func _on_area_shape_entered(area_rid: RID, area: Area3D, area_shape_index: int, local_shape_index: int) -> void:
+	print("OH HAIL NAH" + str(area.get_parent().name))
+	if "PickableBread" in str(area.get_parent().name):
+		breadcount += 1
+		print("Bread Count: " + str(breadcount))
+		if breadcount == 10:
+			self.find_parent("Game").find_child("player").position = Vector3(0.2, 1.5, 24.0)
+
+
+func _on_area_shape_exited(area_rid: RID, area: Area3D, area_shape_index: int, local_shape_index: int) -> void:
+	if "PickableBread" in str(area.get_parent().name):
+		breadcount -= 1
