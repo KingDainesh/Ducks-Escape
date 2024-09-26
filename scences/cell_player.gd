3da988543d29729cafde95ad07529bde5e9a450f
SaxySam <21140@burnside.school.nz>
2024-09-26 14:41:28 +1200

    GameDone

--- scences/cell_player.gd
+++ scences/cell_player.gd
@@ -4,4 +4,5 @@ extends Node3D
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
 	if XRToolsUserSettings.cell_opened == true:
-		$Door.queue_free()
+		if $Door:
+			$Door.queue_free()
--- scripts/MugSpot.gd
+++ scripts/MugSpot.gd
@@ -1,17 +1,19 @@
 extends Area3D
 
-func _on_action_area_shape_exited(area_rid: RID, area: Area3D, area_shape_index: int, local_shape_index: int) -> void:
-	# Check if the book's name is "PickableBook"
-	if area.get_parent().name == "PickableBook":
-		print("The book has departed!")
-		# Find the wall in the scene tree and remove it
-		var wall = get_tree().get_root().find_node("ByeByeWall", true)
-		if wall:
-			wall.queue_free()
-			print("The wall has been removed.")
-		else:
-			print("Wall not found.")
-
 
 func _on_area_shape_entered(area_rid: RID, area: Area3D, area_shape_index: int, local_shape_index: int) -> void:
 	print("BOOK READY")
+
+
+func _on_area_shape_exited(area_rid, area, area_shape_index, local_shape_index):
+	print(area.get_parent().name)
+	if area.get_parent().name == "PickableBook":
+		print("The book has departed!")
+		# Find the wall in the scene tree and remove it
+		if self.find_parent("Game").find_child("ByeByeWall"):
+			var wall = self.find_parent("Game").find_child("ByeByeWall")
+			if wall:
+				wall.queue_free()
+				print("The wall has been removed.")
+			else:
+				print("Wall not found.")
