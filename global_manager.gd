97bb9f1d8c10c85f8c2152daddb7cf3472783ae6
SaxySam <21140@burnside.school.nz>
2024-09-27 13:35:38 +1200

    help

--- global_manager.gd
+++ global_manager.gd
@@ -6,5 +6,5 @@ func set_door_instance(door_node: Node):
 	door_instance = door_node
 
 func hide_door():
-	if door_instance:
-		door_instance.hide_door()
+	if find_parent("Game").find_child("Walls").find_child("Cafe").find_child("Door"):
+		find_parent("Game").find_child("Walls").find_child("Cafe").find_child("Door").queue_free()
